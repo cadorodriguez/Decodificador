178a220ccbd254b7ed913d78fda72ea18b4f55f7
cadorodriguez <cadodesiger@hotmail.com>
2022-12-25 12:59:43 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,30 @@
+var textInput = document.querySelector("#enviar-texto");
+var outInput = document.querySelector("#receber-texto");
+
+function criptografar(){
+
+	var texto = textInput.value;
+
+	var resultadoCriptografado = texto.replace(/e/g, "enter").replace(/i/g, "install").replace(/a/g,"alt").replace(/o/g, "only").replace(/u/g,"ul");
+
+	document.getElementById('output').innerHTML = '<textarea readonly id="enviar-texto">' + resultCripto + 
+  '</textarea>' + '<button class="btn-copiar" id="copiar" onclick="copiar()">Copiar</button>'
+}
+
+function descriptografar(){
+
+	var texto = textInput.value;
+
+	var resultadoDescriptografado = texto.replace(/enter/g, "e").replace(/install/g, "i").replace(/alt/g, "a").replace(/only/g, "o").replace(/ul/g,"u");
+
+	document.getElementById('receber-texto').innerHTML = '<textarea readonly id="enviar-texto">' + resultadoCriptografado + '</textarea>' + '<button class="copiar" id="copiar" onclick="copiar()">Copiar</button>';
+} 
+
+function copiar(){
+
+	var copiaTexto = document.getElementById('enviar-texto');
+
+	copiaTexto.select();
+	document.execCommand('copy');
+	alert("Texto Copiado"); 
+}
