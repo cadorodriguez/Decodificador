4b6a55aefe26843e127215cca52939b07e45ce01
cadorodriguez <cadodesiger@hotmail.com>
2022-12-26 15:00:31 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ function criptografar(){
 
 	var texto = textInput.value;
 
-	var resultadoCriptografado = texto.replace(/e/g, "enter").replace(/i/g, "install").replace(/a/g,"alt").replace(/o/g, "only").replace(/u/g,"ul");
+	var resultadoCriptografado = texto.replace(/e/g, "enter").replace(/i/g, "inst").replace(/a/g,"alt").replace(/o/g, "only").replace(/u/g,"ul");
 
 	document.getElementById('receber-texto').innerHTML = '<textarea readonly id="enviar-texto">' + resultadoCriptografado + 
   '</textarea>' + '<button class="btn-copiar" id="copiar" onclick="copiar()">Copiar</button>'
@@ -15,14 +15,14 @@ function descriptografar(){
 
 	var texto = textInput.value;
 
-	var resultadoDescriptografado = texto.replace(/enter/g, "e").replace(/install/g, "i").replace(/alt/g, "a").replace(/only/g, "o").replace(/ul/g,"u");
+	var resultadoDescriptografado = texto.replace(/enter/g, "e").replace(/inst/g, "i").replace(/alt/g, "a").replace(/only/g, "o").replace(/ul/g,"u");
 
 	document.getElementById('receber-texto').innerHTML = '<textarea readonly id="enviar-texto">' + resultadoDescriptografado + '</textarea>' + '<button class="copiar" id="copiar" onclick="copiar()">Copiar</button>';
 } 
 
 function copiar(){
 
-	var copiaTexto = document.getElementById('copiar');
+	var copiaTexto = document.getElementById('enviar-texto');
 
 	copiaTexto.select();
 	document.execCommand('copy');
